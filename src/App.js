7aa290b0d15a0d641bf36fdf595348380537301f
dngg0016 <dngg0016@student.monash.edu>
2025-05-06 01:22:44 +1000

    ip

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ const [selectedConfig, setSelectedConfig] = useState('ph_temp');
   // Fetch time-series data <button class="citation-flag" data-index="7">
   useEffect(() => {
     // Change '/data.json' to your Flask endpoint
-    fetch('http://localhost:5000/aggregated-data') 
+    fetch('http://3.105.153.220:5000/aggregated-data') 
       .then(response => response.json())
       .then(data => setData(data))
       .catch(error => console.error('Error loading data:', error));
@@ -66,7 +66,7 @@ const [selectedConfig, setSelectedConfig] = useState('ph_temp');
 
   // Fetch data when selected metric changes
   useEffect(() => {
-    fetch(`http://localhost:5000/boxplot-data`)
+    fetch(`http://3.105.153.220:5000/boxplot-data`)
       .then(response => response.json())
       .then(data => setBoxplotData(data))
       .catch(error => console.error('Error:', error));
